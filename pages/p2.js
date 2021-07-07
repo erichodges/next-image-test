eb3a84c2e33422b4af6809849af9960df16facca
Eric Hodges <ehodges2004@gmail.com>
2021-07-07 15:37:09 -0700

    Added page 2 (p2)

---
+++ pages/p2.js
@@ -0,0 +1,17 @@
+import { Image } from 'cloudinary-react';
+
+export default function p2() {
+  return (
+    <div>
+      <div>
+        <h2>Responsive</h2>
+        <Image
+          cloudName='dn3baky3u'
+          publicId='v1576797449/gatsby-cloudinary/evora-gt-front'
+          width='auto'
+          alt='image'
+        />
+      </div>
+    </div>
+  );
+}
