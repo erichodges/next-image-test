90f6db761788f98a3c0025acdbc4e3fe754efcc5
Eric Hodges <ehodges2004@gmail.com>
2021-07-19 15:35:53 -0700

    Chore: cleaned up p2

--- pages/p2.js
+++ pages/p2.js
@@ -7,29 +7,24 @@ export default function Home() {
   return (
     <div className='container'>
       <Head>
-        <title>Create Next App</title>
+        <title>ThemeUI/Next Test</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
         <div>
           <h1 className='title'>
-            Read{' '}
-            <Link href='/posts/first-post'>
-              <a>First Post Page!</a>
+            <Link href='/index'>
+              <a>Home</a>
             </Link>
           </h1>
 
           <p
             sx={{
-              fontSize: '6em',
-              cursor: 'pointer',
-              ':hover': {
-                color: 'accent'
-              }
+              fontSize: '3rem'
             }}
           >
-            TEST
+            Yo duuuuude...
           </p>
           {/*lots and lots of other stuff.....  */}
         </div>
